4425eb77f6b5626c8cbc6ff0b388f7a6ca3729dc
enzomoralesl <enzo_moralesl@yahoo.com.br>
2025-09-15 21:19:38 -0300

    database pool_timeout

--- app/database.py
+++ app/database.py
@@ -4,42 +4,35 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Carrega variáveis de ambiente
+
 load_dotenv()
 
-# Configurações do banco de dados
 POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "12345")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "exampledb")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
-# String de conexão para o SQLAlchemy
 DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-# Configurações do engine do SQLAlchemy
 engine = create_engine(
     DATABASE_URL,
     echo=os.getenv("SQLALCHEMY_ECHO", "True") == "True",
-    pool_pre_ping=True,  # Verifica a conexão antes de usá-la
-    pool_size=10,  # Número máximo de conexões no pool
-    max_overflow=20,  # Número máximo de conexões que podem ser criadas além do pool_size
-    pool_timeout=30  # Timeout de espera por uma conexão do pool
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=40,
+    pool_timeout=60,
+    pool_recycle=1800
 )
-
-# Sessão para consultas no banco
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base para os modelos ORM
 Base = declarative_base()
 
-# Função para criar tabelas
 def create_tables():
     """Cria todas as tabelas definidas nos modelos"""
     from app.models.patient import Patient
     Base.metadata.create_all(bind=engine)
 
-# Dependência para uso nos endpoints FastAPI
 def get_db():
     """Fornece uma sessão de banco de dados para os endpoints FastAPI"""
     db = SessionLocal()
@@ -47,8 +40,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-# Função para criar todas as tabelas definidas nos modelos
-def create_tables():
-    """Cria todas as tabelas no banco de dados"""
-    Base.metadata.create_all(bind=engine)
